19a4695a6f8992948b31a6b6fd6b49721103bf31
Kamiko Loss <info@gloxi.net>
2025-11-14 01:07:21 +0900

    サイコロを振る処理を実装した

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,6 +1,18 @@
 extends Control
 
+var dice_a := 0:
+    set(v):
+        dice_a = v
+        label_dice_a.text = str(v)
+var dice_b := 0:
+    set(v):
+        dice_b = v
+        label_dice_b.text = str(v)
+
 @onready var lanes: HBoxContainer = %Lanes
+
+@onready var button_roll: Button = %ButtonRoll
+@onready var button_end: Button = %ButtonEnd
 
 @onready var label_score: Label = %LabelScore
 @onready var label_target: Label = %LabelTarget
@@ -15,4 +27,19 @@ extends Control
 
 
 func _ready() -> void:
+    button_roll.pressed.connect(_on_button_roll_pressed)
+    button_end.pressed.connect(_on_button_roll_pressed)
+
+
+func _on_button_roll_pressed() -> void:
+    var tween := create_tween()
+    tween.set_loops(10)
+    tween.parallel()
+    tween.tween_callback(func() -> void: dice_a = randi_range(1, 6))
+    tween.tween_callback(func() -> void: dice_b = randi_range(1, 6))
+    tween.chain()
+    tween.tween_interval(0.05)
+
+
+func _on_button_end_pressed() -> void:
     pass
