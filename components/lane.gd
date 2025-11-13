c36a5779598ca42c915ad0f645ba2559a6265af9
Kamiko Loss <info@gloxi.net>
2025-11-14 00:55:11 +0900

    onready を定義した

--- components/lane.gd
+++ components/lane.gd
@@ -1,2 +1,11 @@
 class_name Lane
 extends Control
+
+@onready var buttons: VBoxContainer = %Buttons
+
+@onready var label_score: Label = %LabelScore
+@onready var label_number: Label = %LabelNumber
+
+
+func _ready() -> void:
+    pass
--- scenes/game.gd
+++ scenes/game.gd
@@ -1,1 +1,18 @@
-extends Node2D
+extends Control
+
+@onready var lanes: HBoxContainer = %Lanes
+
+@onready var label_score: Label = %LabelScore
+@onready var label_target: Label = %LabelTarget
+@onready var label_phase: Label = %LabelPhase
+@onready var label_stage: Label = %LabelStage
+@onready var label_dice_a: Label = %LabelDiceA
+@onready var label_dice_b: Label = %LabelDiceB
+
+@onready var texture_rect_reroll_1: TextureRect = %TextureRectReroll1
+@onready var texture_rect_reroll_2: TextureRect = %TextureRectReroll2
+@onready var texture_rect_reroll_3: TextureRect = %TextureRectReroll3
+
+
+func _ready() -> void:
+    pass
